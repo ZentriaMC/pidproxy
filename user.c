dcafc4a66ea13353a425d1a77b72bc8bfe589e64
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-01-01 11:23:33 +0200

    Add GPL 3.0 license

--- user.c
+++ user.c
@@ -1,3 +1,20 @@
+/*
+ * This file is part of pidproxy.
+ * Copyright (c) 2020-2021 Mark Vainomaa.
+ *
+ * pidproxy is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * pidproxy is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with pidproxy. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
