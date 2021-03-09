9b5be8c621456699ee7afaa535e983f3be260341
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-03-09 16:40:08 +0200

    Update license headers

--- user.h
+++ user.h
@@ -1,6 +1,6 @@
 /*
  * This file is part of pidproxy.
- * Copyright (c) 2020-2021 Mark Vainomaa.
+ * Copyright (c) 2020-2021 Zentria OÜ.
  *
  * pidproxy is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
