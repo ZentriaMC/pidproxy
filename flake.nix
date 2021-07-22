e4dc026823223fecc1e9ea09d3c5db5361196a8b
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2021-07-22 15:13:23 +0300

    Add Nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "pidproxy";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      rec {
+        packages.pidproxy = pkgs.callPackage ./. { };
+        packages.pidproxy-static = pkgs.callPackage ./. { enableStatic = true; };
+        defaultPackage = packages.pidproxy;
+      });
+}
